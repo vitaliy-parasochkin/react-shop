fa92d5d9caca872819d8932e2550531d3f4ba746
Vitaliy Parasochkin <parasochkinvitaliy@gmail.com>
2023-04-27 15:31:09 +0300

    added simple page login and hashrouter instead of broswerROuter

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,19 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Products from "../pages/Products";
 import Cart from "../pages/Cart";
 import ErrorPage from "../pages/ErrorPage";
 import MainLayout from "../layouts/MainLayout";
+import Login from "../pages/Login";
 
 export default function Main() {
     return (
-        <BrowserRouter>
+        <HashRouter basename="/react-shop">
             <Routes>
+                <Route path="/" element={<Login />} />
                 <Route element={<MainLayout />}>
                     <Route path="/products" element={<Products />} />
                     <Route path="/cart" element={<Cart />} />
                 </Route>
                 <Route path="/*" element={<ErrorPage />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
