38a8958d4135158d2eff65439567fc20ad9e6773
Vitaliy Parasochkin <parasochkinvitaliy@gmail.com>
2023-04-30 18:50:04 +0300

    refresh react-router-dom

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,21 +1,44 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
 import Products from "../pages/Products";
 import Cart from "../pages/Cart";
 import ErrorPage from "../pages/ErrorPage";
 import MainLayout from "../layouts/MainLayout";
 import Login from "../pages/Login";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            errorElement: <ErrorPage />,
+            children: [
+                {
+                    path: "/login",
+                    element: <Login />,
+                },
+
+                {
+                    path: "/",
+                    element: <MainLayout />,
+                    children: [
+                        {
+                            path: "products",
+                            element: <Products />,
+                        },
+                        {
+                            path: "cart",
+                            element: <Cart />,
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+    {
+        basename: "/react-shop",
+    }
+);
 
 export default function Main() {
-    return (
-        <HashRouter basename="/react-shop">
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/" element={<MainLayout />}>
-                    <Route path="products" element={<Products />} />
-                    <Route path="cart" element={<Cart />} />
-                </Route>
-                <Route path="/*" element={<ErrorPage />} />
-            </Routes>
-        </HashRouter>
-    );
+    return <RouterProvider router={router} />;
 }
