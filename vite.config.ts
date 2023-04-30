3c567dd74954ec85c7a9632aaefc751b98547bb5
Vitaliy Parasochkin <parasochkinvitaliy@gmail.com>
2023-04-30 17:17:58 +0300

    changed vite.config.ts for base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "https://vitaliy-parasochkin.github.io/react-shop",
+    base: "/react-shop/",
 });
